6a4e7c099a5759b4e5862a15815f06b470a8b2a5
AmerRasheed <59695079+AmerRasheed@users.noreply.github.com>
2020-04-25 11:57:26 +0200

    Completed String Exercise

--- App.java
+++ App.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
- * Hello world!
+String World
  *
  */
 public class App 
@@ -10,32 +13,31 @@ public class App
     {
        /*Working with String Exercise by Amer Rasheed Week 2*/
         /*FIRST SEVEN QUESTIONS SOLVED*/
-
-/*What is the length of the String: "Java" ?*/
+//hat is the length of the String: "Java" ?
         String String1="JAVA";
         System.out.println(String1);
         int length_of_string=String1.length();
         System.out.println(length_of_string);  // Solution is 4
 
-/*What char is at index position 6 in the following String:"Long example sentence" ?*/
+//What char is at index position 6 in the following String:"Long example sentence" ?
         String string_position="Long example sentence";
         String  char_at_index_position= string_position.substring(6,7);
         System.out.println(char_at_index_position);    //Solution is x
 
 
-/*What is the index position of 'o' in the following String: "Even longer example sentence" ? */
+//What is the index position of 'o' in the following String: "Even longer example sentence" ?
         String string__index_position="Even longer example sentence";
         int stringpos=string__index_position.indexOf('o');
         System.out.println(stringpos);   // Solution is 6.
                                          //Starting from 0 the string position will be 6
 
-/*Given the following String: "Ok this is not as long!" create a substring of only "not as long" (excluding the exclamation point) and print it out*/
+//Given the following String: "Ok this is not as long!" create a substring of only "not as long" (excluding the exclamation point) and print it out
 
         String st2 = "Ok this is not as long!";
         String targetSt = st2.substring(11,st2.length()-1);
         System.out.println(targetSt); // Solution : not as long
 
-/*Convert the following String: "CAPS EQUALS SCREAMING" to lowercase and print it out. Then convert it back to uppercase and print it out again. */
+//Convert the following String: "CAPS EQUALS SCREAMING" to lowercase and print it out. Then convert it back to uppercase and print it out again.
 
         String st3="CAPS EQUALS SCREAMING";
         String lower_case_St = st3.toLowerCase();
@@ -44,10 +46,10 @@ public class App
         System.out.println("The upper case sentence is " + st3);                //caps equals screaming
 
 
-/*Correct the following String: "Java is the worst programming language!" by replacing the (obviously incorrect) word "worst" with the word "best". Then print out the sentence.*/
+//Correct the following String: "Java is the worst programming language!" by replacing the (obviously incorrect) word "worst" with the word "best". Then print out the sentence.
 
         String st4="Java is the worst programming language!";
-        String stringarray[] = st4.split(" ");
+        String stringarray[] = st4.split(" ");                          //split function separates the string w.r.t. spaces in st4
         for(int i=0;i<stringarray.length;i++)
           System.out.print(stringarray[i] + " ");
 
@@ -60,12 +62,12 @@ public class App
             System.out.print(stringarray[i] + " ");
 
 
-/*What is the output of the following String: "\tJ\ta\tv\ta\t" after you trim it? */
+//What is the output of the following String: "\tJ\ta\tv\ta\t" after you trim it?
 
             String stTrim = "\tJ\ta\tv\ta\t";
             System.out.println(stTrim.trim()); //Java
 
-/*Parse the following int: 20 to a String and add a 20 to the end of the String. Printing it out should return: "2020" */
+//Parse the following int: 20 to a String and add a 20 to the end of the String. Printing it out should return: "2020"
             String stInt="";
             int pass=20;
             String newString = String.valueOf(pass);
@@ -74,10 +76,48 @@ public class App
             stInt1 = stInt.concat(newString);
             System.out.println(stInt1);
 
+        String st9="Oil and Water";
+        String words[] = st9.split(" ");                             //split function separates the string w.r.t. spaces in st4
+        String splitarray[] = new String[3];
+        for(int i=0;i<words.length;i++)                              // Printing array contents :: Oil and Water
+            System.out.print(words[i] + " ");
+
+        for(int i=0;i<words.length;i++){
+                  if (words[i].equalsIgnoreCase("and"))
+                    splitarray[i] = "";                              // condition separating Oil and water
+                  else                                               //and storing in separate String array
+                      splitarray[i] = words[i];
+         }
+        System.out.println("\n");
+
+        for(int i=0;i<splitarray.length;i++)                         // Output will be "Oil"
+            System.out.println(splitarray[i] + " ");                 //"Water"
+
+//Split the following String: "Carl,Susie,Fredrick,Bob,Erik" into an array. Print out all names separately
+
+String st5="Carl, Susie, Fredrick,Bob,Erik";
+String[] anarray = st5.split(",");
+        for(int i=0;i<anarray.length;i++)                              // Printing array contents :: Carl Susie Fredrick Bob Erik
+            System.out.print(anarray[i] + " ");
+        System.out.println("\n");
+
+// Convert the following String: "ThisShouldBeConverted" to a char array. Iterate through the char array and print out each element
+        String st6="ThisShouldBeConverted";
+        char[] chararray    = new char[st6.length()]; // declaring a char array of length equivalent to string
+
+        for(int i=0;i<chararray.length;i++)
+            chararray[i]=st6.charAt(i);               // assigns each element in the string at i position to equivalent position in chararray
+
+        for(int i=0;i<chararray.length;i++)
+            System.out.print(chararray[i] + " ");
+        System.out.println("\n");
+
+// Another way is by using function toChararray          chararray=st6.toCharArray();
 
 
-
-
-
+//Convert the following char[]: {'J','a','v','a'} to a String and print it out.
+        char[] javacharArray = {'J','a','v','a'};
+        System.out.println(Arrays.toString(javacharArray));   //Arrays utility function toString converts char array to String
+                                                              //toString convert array to STRING
     }
 }
